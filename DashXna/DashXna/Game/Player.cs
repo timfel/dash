4916ad335d69f88ec9ef6a071dfb1b18eb715b1e
Markus Kahl <machisuji@googlemail.com>
2012-07-15 14:08:39 +0200

    made dash 1/3 smaller and put buttons into the game

--- DashXna/DashXna/Game/Player.cs
+++ DashXna/DashXna/Game/Player.cs
@@ -34,13 +34,15 @@ namespace Dash
         {
             spriteBatch = new SpriteBatch(Game.GraphicsDevice);
 
-            sprites.Add("dash", new Frame(new Microsoft.Xna.Framework.Rectangle(0, 0, 300, 342), Game.Content.Load<Texture2D>("Dash")));
-            sprites.Add("dash1", new Frame(new Microsoft.Xna.Framework.Rectangle(0, 0, 300, 342), Game.Content.Load<Texture2D>("Dash1")));
-            sprites.Add("dash2", new Frame(new Microsoft.Xna.Framework.Rectangle(0, 0, 300, 341), Game.Content.Load<Texture2D>("Dash2")));
+            sprites.Add("dash", new Frame(new Microsoft.Xna.Framework.Rectangle(0, 0, 200, 228), Game.Content.Load<Texture2D>("Dash")));
+            sprites.Add("dash1", new Frame(new Microsoft.Xna.Framework.Rectangle(0, 0, 200, 228), Game.Content.Load<Texture2D>("Dash1")));
+            sprites.Add("dash2", new Frame(new Microsoft.Xna.Framework.Rectangle(0, 0, 200, 227), Game.Content.Load<Texture2D>("Dash2")));
 
-            sprites.Add("dash-ducked", new Frame(new Microsoft.Xna.Framework.Rectangle(0, 0, 377, 230), Game.Content.Load<Texture2D>("Dash-Ducked-Sliding")));
+            sprites.Add("dash-ducked", new Frame(new Microsoft.Xna.Framework.Rectangle(0, 0, 251, 153), Game.Content.Load<Texture2D>("Dash-Ducked-Sliding")));
 
             sprites.Add("broom", new Frame(new Microsoft.Xna.Framework.Rectangle(0, 0, 238, 88), Game.Content.Load<Texture2D>("Besen")));
+
+            sprites.Add("buttons", new Frame(new Microsoft.Xna.Framework.Rectangle(0, 0, 800, 480), Game.Content.Load<Texture2D>("Buttons")));
 
             defaultAnimation.Add(sprites["dash1"].Copy());
             defaultAnimation.Add(sprites["dash"].Copy());
@@ -59,7 +61,8 @@ namespace Dash
             duckAnimation.Add(sprites["dash-ducked"].Copy());
             duckAnimation.Add(sprites["dash-ducked"].Copy());
 
-            pos.Y = 450;
+            pos.X = 50;
+            pos.Y = 370;
             
             base.LoadContent();
         }
@@ -88,10 +91,13 @@ namespace Dash
             TouchCollection touchCollection = TouchPanel.GetState();
             foreach (TouchLocation tl in touchCollection)
             {
-                if ((tl.State == TouchLocationState.Pressed)
-                        || (tl.State == TouchLocationState.Moved))
+                if (tl.State == TouchLocationState.Pressed && tl.Position.X >= 0 && tl.Position.X <= 100 && tl.Position.Y >= 400)
                 {
                     Jump();
+                }
+                else if (tl.State == TouchLocationState.Pressed && tl.Position.X >= 700 && tl.Position.X <= 800 && tl.Position.Y >= 400)
+                {
+                    Duck();
                 }
             }
         }
@@ -124,6 +130,10 @@ namespace Dash
                 (int)pos.X + frame.offsetX,
                 (int)pos.Y + frame.offsetY - frame.bounds.Height,
                 frame.bounds.Width, frame.bounds.Height), Color.White);
+
+            var buttons = sprites["buttons"];
+
+            spriteBatch.Draw(buttons.texture, buttons.bounds, Color.White);
 
             spriteBatch.End();
             
